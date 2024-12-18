07536620e1246ba5eb857b746ec63ba035690f84
Nikola Kirov <nikolakrasimirovkirov@gmail.com>
2024-12-19 01:34:27 +0200

    Update app configuration and routes for Firebase integration

--- JsWeb_Angular_SoftUniExam/src/app/app.config.ts
+++ JsWeb_Angular_SoftUniExam/src/app/app.config.ts
@@ -5,12 +5,14 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { firebaseConfig } from '../environments/environment'; // Import your environment for Firebase config
 import { provideAuth, getAuth } from '@angular/fire/auth'; // Import getAuth for providing Auth
 import { provideFirestore, getFirestore } from '@angular/fire/firestore'; // Import getFirestore for Firestore
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), // Provide routes directly
     provideFirebaseApp(() => initializeApp(firebaseConfig)), // Initialize Firebase with the configuration
     provideAuth(() => getAuth()), // Provide Firebase Authentication with the required function
-    provideFirestore(() => getFirestore()) // Provide Firestore with the required function
+    provideFirestore(() => getFirestore()), // Provide Firestore with the required function
+    provideHttpClient(),
   ]
 };
--- JsWeb_Angular_SoftUniExam/src/app/app.routes.ts
+++ JsWeb_Angular_SoftUniExam/src/app/app.routes.ts
@@ -2,10 +2,23 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { CreateComponent } from './movies/create/create.component';
+import { DetailsComponent } from './movies/details/details.component'; // Import DetailsComponent
+import { AuthGuard } from './core/guards/auth.guard';
+import { EditComponent } from './movies/edit/edit.component';
 
 export const routes: Routes = [
     { path: 'auth/login', component: LoginComponent },
     { path: 'auth/register', component: RegisterComponent },
-    { path: 'catalog', component: CatalogComponent },
-    { path: '', redirectTo: '/auth/login', pathMatch: 'full' }, // Important: Add default route
+    {
+        path: 'movies',
+        children: [
+            { path: 'catalog', component: CatalogComponent },
+            { path: 'create', component: CreateComponent, canActivate: [AuthGuard] }, // Protected route
+            { path: ':id', component: DetailsComponent, canActivate: [AuthGuard] }, // Protected route
+            { path: ':id/edit', component: EditComponent, canActivate: [AuthGuard] }, // Add this route
+            { path: '', redirectTo: 'catalog', pathMatch: 'full' }
+        ]
+    },
+    { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
 ];
