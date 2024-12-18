7bbf845275ba74361f7a8e9c17964b8a97e0ce4b
Nikola Kirov <nikolakrasimirovkirov@gmail.com>
2024-12-18 12:21:22 +0200

    Add environment configuration

---
+++ JsWeb_Angular_SoftUniExam/src/environments/environment.ts
@@ -0,0 +1,30 @@
+// Since this is a SoftUni exam project, I include the Firebase 
+// configuration with API keys in the repository. Here's why:
+
+// From the exam requirements:
+// This is an exam project, not a production application
+// Alternative approaches like using environment variables or separate config 
+// files would make the examination take longer
+
+// Make it harder for the examiner to test your project
+// Take up valuable presentation time with setup
+// Risk technical issues during defense
+// So for this specific academic context, I am keep the firebase.config.ts 
+// file with API keys in my repository. After your exam is complete,
+// I will delete the Firebase project.
+
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyDcljVvxmb8jY7sEwgODzN8wkX8VJitBtk",
+
+    authDomain: "jswebangularsoftuniexambackend.firebaseapp.com",
+
+    projectId: "jswebangularsoftuniexambackend",
+
+    storageBucket: "jswebangularsoftuniexambackend.firebasestorage.app",
+
+    messagingSenderId: "1069362064851",
+
+    appId: "1:1069362064851:web:805f7a389de74d3366d326"
+
+};
